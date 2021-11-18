abb64d7d959fa11935a8709946c332f62654afdb
SaN4OuSl <oleksandr.laptiev@nure.ua>
2021-11-18 17:13:08 +0200

    delete flywaydb and add some methods for rest controllers

--- soft_for_farm/src/main/java/org/example/exception/jwt/JwtAuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package org.example.exception.jwt;
-
-public class JwtAuthenticationException extends Throwable {
-    public JwtAuthenticationException(String message) {
-        super(message);
-    }
-}
--- soft_for_farm/src/main/java/org/example/repository/auth/UserRepository.java
+++ soft_for_farm/src/main/java/org/example/repository/auth/UserRepository.java
@@ -1,13 +1,19 @@
 package org.example.repository.auth;
 
 import org.example.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByLogin(String login);
-    
+
     User findUserById(Long id);
+
+    @Query("select u from User u")
+    Page<User> findAllUsers(User user, Pageable pageable);
 
     boolean existsUserByLogin(String login);
 }

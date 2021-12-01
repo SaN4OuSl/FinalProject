a39fdc79f9d3115f5cf52e191a3f63e24ddeb3a0
SaN4OuSl <oleksandr.laptiev@nure.ua>
2021-12-01 11:08:57 +0200

    change Eager to Lazy

--- soft_for_farm/src/main/java/org/example/entity/Animal.java
+++ soft_for_farm/src/main/java/org/example/entity/Animal.java
@@ -45,7 +45,7 @@ public class Animal {
     @DecimalMin("0.00")
     private Double costOfOneAnimal;
     
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false, name = "farm_id")
     @JsonIgnore
     private Farm farm;
